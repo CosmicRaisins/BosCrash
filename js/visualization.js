e16f2525463654cebb89408081b4a8398d637756
CosmicRaisins <60367004+CosmicRaisins@users.noreply.github.com>
2020-11-10 16:00:31 -0500

    Test line chart implementation
    Uploaded test data
    Testing line chart implementation
    No formatting yet

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,22 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
-((() => {
 
-  console.log('Hello, world!');
+// load data from csv
+// TODO: Update Data Column Names
+// TODO: More Cleaning
+d3.csv("data/TestingData.json").then(data => {
 
-})());
+  // General event type for selections, used by d3-dispatch
+  // https://github.com/d3/d3-dispatch
+  const dispatchString = 'selectionUpdated';
+
+  // Create a line chart given x and y attributes, labels, offsets;
+  // a dispatcher (d3-dispatch) for selection events;
+  // a div id selector to put our svg in; and the data to use.
+  let hourlyLineChart = linechart()
+    .x(d => d.hour)
+    .xLabel("Hour")
+    .y(d => d.records)
+    .yLabel('Number or Crashes')
+    .yLabelOffset(40)
+    .selectionDispatcher(d3.dispatch(dispatchString))
+    ("#vis-svg-1", data);
+})
