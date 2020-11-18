6e3bfe73b41628fa604d92abb21a429b332bf194
Jiaming-Hu <42947686+Jiaming-Hu@users.noreply.github.com>
2020-11-17 23:22:51 -0500

    Add files via upload

--- js/barchart.js
+++ js/barchart.js
@@ -1,208 +1,135 @@
-// /* global D3 */
+let margin = {
+    top: 100,
+    left: 50,
+    right: 30,
+    bottom: 0
+  },
+  width = 900 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-// // Initialize a line chart. Modeled after Mike Bostock's
-// // Reusable Chart framework https://bost.ocks.org/mike/chart/
-// function barchart() {
+//Define data for bar chart
+d3.csv('data/weather.csv', function(d) {
+  return {
+    weather: d.Weather,
+    count: +d.Count
+  };
+}).then(barChart);
 
-//   // Based on Mike Bostock's margin convention
-//   // https://bl.ocks.org/mbostock/3019563
+// function to draw bar chart
+function barChart(data) {
 
-//   let margin = {
-//       top: 60,
-//       left: 50,
-//       right: 30,
-//       bottom: 35
-//     },
-//     title = "Default Title",
-//     width = 500 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom,
-//     xValue = d => d[0],
-//     yValue = d => d[1],
-//     xLabelText = '',
-//     yLabelText = '',
-//     yLabelOffsetPx = 0,
-//     xScale = d3.scaleBand(),
-//     yScale = d3.scaleLinear(),
-//     ourBrush = null,
-//     selectableElements = d3.select(null),
-//     dispatcher;
+  // choose color of the bar based on the country's continent
+  function chooseColor(w) {
+    if(w == 'Clear') {
+      return "#00FFFF";
+    }
+    if(w == 'Cloudy') {
+      return "#5F9EA0";
+    }
+    if(w == 'Rain') {
+      return '#A9A9A9';
+    }
+    if(w == 'Cloudy/Rain') {
+      return '#696969';
+    }
+    if(w == 'Snow') {
+      return '#D3D3D3';
+    }
+    return "#000080";
+  }
 
-//   // Create the chart by adding an svg to the div with the id
-//   // specified by the selector using the given data
-//   function chart(selector, data) {
-//     let svg = d3.select(selector)
-//       .append('svg')
-//       .attr('preserveAspectRatio', 'xMidYMid meet')
-//       .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
-//       .classed('svg-content', true);
-
-//     svg = svg.append('g')
-//       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-//     //Define scales
-//     let xScale = d3.scaleBand()
-//       .range([0, width - margin.right-50])
-//       .domain()
-//       .padding(.1)
- 
-//     yScale
-//       .domain([
-//         d3.min(data, d => yValue(d)),
-//         d3.max(data, d => yValue(d))
-//       ])
-//       .rangeRound([height, 0]);
-
-//     // X axis
-//     let xAxis = svg.append('g')
-//       .attr('transform', 'translate(0,' + (height + 35) + ')')
-//       .call(d3.axisBottom(xScale));
-//     //TODO: does not work
-//         // .ticks(0, 22, 12));
-
-//     // X axis label
-//     xAxis.append('text')
-//       .attr('class', 'axisLabel')
-//       .attr('transform', 'translate(' + (width - 70) + ',-10)')
-//       .text(xLabelText);
-
-//     // Y axis label, act like the title for the graph
-//     let yAxis = svg.append('text')
-//       .attr('class', 'axisLabel')
-//       .attr('transform', 'translate(' + yLabelOffsetPx + ', 0)')
-//       .text(yLabelText);
+  // first visualization
+  let svg1 = d3.select('#vis-svg-2')
+  .append('svg')
+  .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of the page.
+  .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
+  .style('background-color', '#ccc') // change the background color to white
+  .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+  .classed('svg-content', true);
 
 
-//     // Add the rectangle
-//     let bar = svg.selectAll('rect')
-//       .data(data)
-//       .enter()
-//       .append('rect')
-//       .attr('x', X)
-//       .attr('y', Y)
-//       .attr('width', 5)
-//       .attr('height', 100);
+  // vertical scale
+  let yScale = d3.scaleLinear()
+    .domain([1000, 105000])
+    .range([height - margin.bottom, margin.top]);
 
-//      svg.call(brush);
-    
-//     // // Highlight points when brushed
-//      function brush(g) {
-//        const brush = d3.brush()
-//          .on('start brush', highlight)
-//          .on('end', brushEnd)
-//          .extent([
-//            [-margin.left, -margin.bottom],
-//            [width + margin.right, height + margin.top]
-//          ]);
-    
-//        ourBrush = brush;
-    
-//        g.call(brush); // Adds the brush to this element
-    
-//     //   // Highlight the selected circles.
-//        function highlight(event, d) {
-//          if (event.selection === null) return;
-//          const [
-//            [x0, y0],
-//            [x1, y1]
-//          ] = event.selection;
-//          points.classed('selected', d =>
-//            x0 <= X(d) && X(d) <= x1 && y0 <= Y(d) && Y(d) <= y1
-//          );
-    
-//     //     // Get the name of our dispatcher's event
-//          let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
-//     //
-//     //     // Let other charts know
-//          dispatcher.call(dispatchString, this, svg.selectAll('.selected').data());
-//        }
-//     //
-//        function brushEnd(event, d) {
-//          // We don't want infinite recursion
-//          if(event.sourceEvent !== undefined && event.sourceEvent.type!='end'){
-//            d3.select(this).call(brush.move, null);
-//          }
-//        }
-//      }
+  // horizontal scale
+  let xScale = d3.scaleBand()
+    .domain(
+      data.map(function(d) {
+        return d.weather;
+      })
+    )
+    .range([margin.left, width - margin.right])
+    .padding(0.4);
 
-//     return chart;
-//   }
+  // add y-axis
+  let yAxisBar = svg1
+    .append('g')
+    .attr('transform', `translate(${margin.left},0)`)
+    .call(d3.axisLeft().scale(yScale))
 
-//   // The x-accessor from the datum
-//   function X(d) {
-//     return xScale(xValue(d));
-//   }
+    //Add label
+    .append('text')
+    .attr('y', 80)
+    .attr('x', 130)
+    .style('stroke', 'black')
+    .style('font-size', '20px')
+    .text('Number of Crashes');
 
-//   // The y-accessor from the datum
-//   function Y(d) {
-//     return yScale(yValue(d));
-//   }
+  // add x-axis
+  let xAxisBar = svg1
+    .append('g')
+    .attr('transform', `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom().scale(xScale))
+    .selectAll('text')
+    .style('text-anchor', 'end')
+    .attr('dx', '0.5em')
+    .attr('dy', '0.1em')
+    .attr('transform', 'rotate(-30)')
 
-//   chart.margin = function (_) {
-//     if (!arguments.length) return margin;
-//     margin = _;
-//     return chart;
-//   };
 
-//   chart.width = function (_) {
-//     if (!arguments.length) return width;
-//     width = _;
-//     return chart;
-//   };
+    //Add label
+    .append('text')
+    .attr('x', width)
+    .attr('y', -10)
+    .style('stroke', 'black')
+    .style('font-size', '20px')
+    .text('Weather Condition');
 
-//   chart.height = function (_) {
-//     if (!arguments.length) return height;
-//     height = _;
-//     return chart;
-//   };
+  //Draw bars
+  let barChart = svg1
+    .selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', function(d) {
+      return xScale(d.weather);
+    })
+    .attr('y', function(d) {
+      return yScale(d.count);
+    })
+    .attr('width', xScale.bandwidth())
+    .attr('fill', function(d) {return chooseColor(d.weather)})
+    .attr('height', function(d) {
+      return height - margin.bottom - yScale(d.count);
+    })
+    .on('mouseenter', function(d) {
+      d3.select(this).attr('opacity', 0.5)
+    })
+    .on('mouseleave', function(d) {
+      d3.select(this).attr('opacity', 1)
+    });
 
-//   chart.x = function (_) {
-//     if (!arguments.length) return xValue;
-//     xValue = _;
-//     return chart;
-//   };
+    // add title of the visualization
+  svg1.append('text')
+    .attr('x', 260)
+    .attr('y', 80)
+    .attr('text-anchor', 'right')
+    .text('Number of Crashes Under Different Weather Condition')
+    .style('font-color', 'white')
+    .style('font-size', '25px');
 
-//   chart.y = function (_) {
-//     if (!arguments.length) return yValue;
-//     yValue = _;
-//     return chart;
-//   };
 
-//   chart.xLabel = function (_) {
-//     if (!arguments.length) return xLabelText;
-//     xLabelText = _;
-//     return chart;
-//   };
 
-//   chart.yLabel = function (_) {
-//     if (!arguments.length) return yLabelText;
-//     yLabelText = _;
-//     return chart;
-//   };
-
-//   chart.yLabelOffset = function (_) {
-//     if (!arguments.length) return yLabelOffsetPx;
-//     yLabelOffsetPx = _;
-//     return chart;
-//   };
-
-//   // TODO: Brushing temporarily removed.
-//   // Gets or sets the dispatcher we use for selection events
-//   chart.selectionDispatcher = function (_) {
-//     if (!arguments.length) return dispatcher;
-//     dispatcher = _;
-//     return chart;
-//   };
-
-//   // Given selected data from another visualization
-//   // select the relevant elements here (linking)
-//   chart.updateSelection = function (selectedData) {
-//     if (!arguments.length) return;
-
-//     // Select an element if its datum was selected
-//     selectableElements.classed('selected', d =>
-//       selectedData.includes(d)
-//     );
-//   };
-
-//   return chart;
-// }
+}
